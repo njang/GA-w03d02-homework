5ad2b9c69e3f737cd10e6f1720496f49ea7820da
njang <neo.w.jang@gmail.com>
2017-12-19 17:31:15 -0600

    Complete AJAX get from fixer.io

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 function convertCurrency(event) {
-  if ('type' in event.target && event.target.type === 'button') {
-    console.log('handle currency conversion here');
-  }
+	if ('type' in event.target && event.target.type === 'button') {
+		console.log('handle currency conversion here');
+	}
+
+	$.get("https://api.fixer.io/latest?base=USD&symbols=KRW", function(response){
+		console.log(Number($('#inputCurrency').text()));
+		console.log(response.rates);
+	});
 }
 
 document.body.addEventListener('click', convertCurrency);
