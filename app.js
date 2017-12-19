75152635912c029b0590860bedda418baba39905
njang <neo.w.jang@gmail.com>
2017-12-19 16:51:32 -0600

    Add starter code to html and js files

---
+++ app.js
@@ -0,0 +1,7 @@
+function convertCurrency(event) {
+  if ('type' in event.target && event.target.type === 'button') {
+    console.log('handle currency conversion here');
+  }
+}
+
+document.body.addEventListener('click', convertCurrency);
