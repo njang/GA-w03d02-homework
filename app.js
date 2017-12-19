3c31e8bf59ae5ef842f195cb79b51103d966165b
njang <neo.w.jang@gmail.com>
2017-12-19 17:58:17 -0600

    Complete USD conversion to KRW

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 function convertCurrency(event) {
 	if ('type' in event.target && event.target.type === 'button') {
 		console.log('handle currency conversion here');
+		// $.get("https://api.fixer.io/latest?base=USD&symbols=KRW", function(response){
+		$.get("https://api.fixer.io/latest?base=USD&symbols=KRW", function(response){	
+			let currencyEntered = Number($('#inputCurrency').val());
+			let conversionRate = Number(response.rates.KRW);
+			let currencyConverted = currencyEntered * conversionRate;
+			
+			$('#output_amount').html(currencyConverted);
+			$('#output_currency').html("KRW");
+		});
 	}
-
-	$.get("https://api.fixer.io/latest?base=USD&symbols=KRW", function(response){
-		console.log(Number($('#inputCurrency').text()));
-		console.log(response.rates);
-	});
 }
 
 document.body.addEventListener('click', convertCurrency);
