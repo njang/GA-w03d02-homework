d93cb8822e02c00436911ac8b2bce2cdd1ac4f31
njang <neo.w.jang@gmail.com>
2017-12-20 01:10:04 -0600

    Complete functional requirement

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-function convertCurrency(event) {
-	if ('type' in event.target && event.target.type === 'button') {
-		console.log('handle currency conversion here');
-		// $.get("https://api.fixer.io/latest?base=USD&symbols=KRW", function(response){
-		$.get("https://api.fixer.io/latest?base=USD&symbols=KRW", function(response){	
-			let currencyEntered = Number($('#inputCurrency').val());
-			let conversionRate = Number(response.rates.KRW);
-			let currencyConverted = currencyEntered * conversionRate;
-			
-			$('#output_amount').html(currencyConverted);
-			$('#output_currency').html("KRW");
-		});
-	}
-}
 
+let rates;
+$.get("https://api.fixer.io/latest?base=USD", function(response){	
+	// Isolate the rates from the response.
+	rates = response.rates;
+	// Collect all availalbe rates by names.
+	ratesAvailable = Object.keys(rates);
+	// Construct the list of options for the available currencies.
+	for (let i = 0; i < ratesAvailable.length; i ++) {
+		// Create an option element. Set the value and display text to the currency name.
+		let optionElement = document.createElement('option');
+		optionElement.value = ratesAvailable[i];
+		optionElement.text = ratesAvailable[i];
+		// Append the option element to the select tag with targetCurrencies id.
+		document.getElementById('targetCurrencies').appendChild(optionElement);
+	}
+});
+
+function convertCurrency(event) {
+  if ('type' in event.target && event.target.type === 'button') {
+    // Retrieve target currency from the form, and convert it to string.
+    let targetCurrency = $('#targetCurrency').val().toString();
+    // Retrieve input amount from the form.
+    let inputAmount = $('#inputAmount').val();
+    // Convert the value, and keep two digits below decimal point.
+    let outputAmount = Math.round(inputAmount * rates[targetCurrency] * 100) / 100;
+	// Display the results.
+	$('#output').html(`${inputAmount} USD is ${outputAmount.toLocaleString()} ${targetCurrency}.`);
+  }
+}
 document.body.addEventListener('click', convertCurrency);
